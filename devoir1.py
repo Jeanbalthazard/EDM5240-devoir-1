5b7175584451ea607171c49dc53f950d88c28a6a
Jeanbalthazard <balthazardjean@gmail.com>
2017-02-13 21:12:41 -0500

    Update devoir1.py

--- devoir1.py
+++ devoir1.py
@@ -3,7 +3,7 @@
 liste1 = list(range(30000,100000))
 # J'ai créé une liste couvrant les numéros de permis pour les médecins admis entre 1930 et 1999.
 
-for liste2 in range(0,17000):
+for liste2 in range(0,18000):
     print(liste1, "{0:0>5}".format(liste2)) 
 
 # J'ai créé un « loop » couvrant les numéros de permis pour les médecins admis entre 2000 et 2017. 
